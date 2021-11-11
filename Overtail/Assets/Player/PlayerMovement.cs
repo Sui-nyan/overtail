d170fe0ec0fd87959e5d5285b265a134aa64b9d6
Felix <fs146@hdm-stuttgart.de>
2021-11-11 09:48:13 +0100

    Fix some names and walking speed

--- Overtail/Assets/Player/PlayerMovement.cs
+++ Overtail/Assets/Player/PlayerMovement.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementFix : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed;
     public Rigidbody2D rb;
     public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-    }
-
-    // FixedUpdate is called 50 times per second
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);      // fixedDeltaTime == time since last function call
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
     }
 }
