034495ff8caa58c87349ad43de1e423b056f4424
Felix <fs146@hdm-stuttgart.de>
2021-11-18 09:20:33 +0100

    fix animation state
    idle animation changes to walking animation

--- Overtail/Assets/Player/PlayerMovement.cs
+++ Overtail/Assets/Player/PlayerMovement.cs
@@ -7,17 +7,29 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     public Vector2 movement;
+    public Animator animator;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void FixedUpdate()
+    {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
+        if (movement.x != 0 || movement.y != 0) {
+            animator.SetBool("isWalking", true);
+            rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        } else {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
