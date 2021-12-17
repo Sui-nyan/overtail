f9119beaef7042d5bc59e335bab0eaa58dbc009b
peanutted <78491387+peanutted@users.noreply.github.com>
2021-12-17 13:26:04 +0100

    DialogueObject and Typewritingeffect
    DialogueObjects can now be added and the dialogue appears in a typewriting effect

--- Overtail/Assets/Scripts/DialogueUI.cs
+++ Overtail/Assets/Scripts/DialogueUI.cs
@@ -5,10 +5,39 @@ using UnityEngine;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject test;
+
+    private TextWriter textWriter;
 
     private void Start()
     {
-        textLabel.text = "Hello World! \n How are you?";
+        textWriter = GetComponent<TextWriter>();
+        CloseDialogue();
+        StartDialogue(test);
+    }
+
+    public void StartDialogue(DialogueObject dialogueObject)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return textWriter.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.anyKeyDown);
+        }
+
+        CloseDialogue();
+    }
+
+    private void CloseDialogue()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
     }
 }
---
+++ Overtail/Assets/Scripts/TextWriter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextWriter : MonoBehaviour
+{
+    [SerializeField] private float writingSpeed = 0.1f;
+
+    public Coroutine Run(string textToType, TMP_Text textLabel)
+    {
+        return StartCoroutine(TypeText(textToType,textLabel));
+    }
+
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+        textLabel.text = string.Empty;
+
+        for( int i = 0; i < textToType.Length; i++)
+        {
+            textLabel.text += textToType[i];
+            yield return new WaitForSeconds(writingSpeed);
+        }
+
+    }
+}
