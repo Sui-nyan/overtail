1bc6a4f13f31ce6d067121157fa9a8333f8c4f77
kd049 <kd049@hdm-stuttgart.de>
2021-12-17 12:10:50 +0100

    Added Documentation and overloaded Methods

--- Overtail/Assets/Scripts/Camera/CameraFollow.cs
+++ Overtail/Assets/Scripts/Camera/CameraFollow.cs
@@ -5,51 +5,84 @@ using Overtail;
 
 namespace Overtail.Camera
 {
+    /// <summary>
+    /// Class to smoothly follow a certain target continously.
+    /// Can be assigned in Unity Inspector. (Defaults to <see cref="GameObject"/> with "Player" tag)
+    /// <para /><see cref="currentTarget"/> target to be followed
+    /// <para /><see cref="maxOffset"/> maximum distance before camera starts following
+    /// <para /><see cref="smoothTime"/> camera move speed 
+    /// </summary>
     public class CameraFollow : Camera
     {
-        [SerializeField] private GameObject target;
-        [SerializeField] private float maxOffset;
-        [SerializeField] public float smoothTime = 0.25f;
-        private Vector3 velocity = Vector3.zero;
-        public Vector2 TargetVector => target.transform.position - transform.position;
-        public float Distance => TargetVector.magnitude;
+        private Vector3 velocity = Vector3.zero;            // reference vector for SmoothDamp()
 
-        void Start()
+        [SerializeField] protected GameObject currentTarget;         // target to follow
+        [SerializeField] protected float maxOffset;           // offset, zero => character will always be centered
+        [SerializeField] protected float smoothTime = 0.25f;   // time it takes camera to reach target position. Zero is instant refocus
+
+
+        protected void Start()
         {
-            base.Start();
-            if (target == null)
+            if (currentTarget == null)
             {
-                target = FindPlayerObject();
-                Debug.Log("No camera target found. Default to <Player>" + target);
+                currentTarget = FindPlayerObject();
+                Debug.Log("No camera target found. Defaulted to <Player>" + currentTarget);
             }
         }
 
         void LateUpdate()
         {
-            if (Distance > maxOffset)
-            {
-                Follow();
-            }
+            Follow();
+
         }
 
-        public void Follow(GameObject targetObj)
+        public void SetTarget(GameObject newTarget)
         {
+            currentTarget = newTarget;
+        }
+
+        public Vector2 TargetVector()
+        {
+            return TargetVector(currentTarget);
+        }
+        public Vector2 TargetVector(GameObject myTarget)
+        {
+            return myTarget.transform.position - transform.position;
+        }
+        public float Distance(GameObject toTarget)
+        {
+            return TargetVector(toTarget).magnitude;
+        }
+
+        public float Distance()
+        {
+            return TargetVector().magnitude;
+        }
+        public void Follow(GameObject targetObj, float cameraSpeed)
+        {
+            if (Distance() < maxOffset) return;
             Vector3 _targetPos = targetObj.transform.position;
             Vector3 _myPos = transform.position;
 
             Vector3 destination = new Vector3(_targetPos.x, _targetPos.y, _myPos.z);
-            SetCamera(Vector3.SmoothDamp(Position3D, destination, ref velocity, smoothTime));
+            SetCamera(Vector3.SmoothDamp(Position3D, destination, ref velocity, cameraSpeed));
+        }
+        public void Follow(GameObject targetObj)
+        {
+            Follow(targetObj, smoothTime);
         }
 
         public void Follow()
         {
-            Follow(target);
+            Follow(currentTarget);
         }
 
         public GameObject FindPlayerObject()
         {
-            Player p = FindObjectOfType(typeof(Player)) as Player;
-            return p.gameObject;
+            Debug.Log($"CameraFollow> {GameObject.FindGameObjectWithTag("Player")}");
+            return GameObject.FindGameObjectWithTag("Player");
+            // Player p = FindObjectOfType(typeof(Player)) as Player;
+            // return p.gameObject;
         }
     }
 }
