fc7832a6c93ef911b7b9561eb68b3182ff05581b
banti <mb365@hdm-stuttgart.de>
2021-12-22 13:14:59 +0100

    Interface and Player Movement
    Basic arcade Interface with code of two PLayer Playermovement - need to be changed to single, just for test right now

---
+++ Overtail/Assets/MiniGame/Ping-Pong/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPong : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rg;
+    public bool left;
+    void Update()
+    {
+        if (left)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                rg.velocity = new Vector2(0f, speed);
+            } else if (Input.GetKey(KeyCode.S))
+            {
+                rg.velocity = new Vector2(0f, -speed);
+            } else 
+            {
+                rg.velocity = new Vector2(0f, 0f);
+            }
+        } else
+        {
+            if (left)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    rg.velocity = new Vector2(0f, speed);
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    rg.velocity = new Vector2(0f, -speed);
+                }
+                else
+                {
+                    rg.velocity = new Vector2(0f, 0f);
+                }
+            }
+        }
+    }
+}
