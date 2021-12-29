9917e8d607741efb307fdd66ab421408c34639f8
peanutted <78491387+peanutted@users.noreply.github.com>
2021-12-29 21:33:33 +0100

    Idk random things pls don't judge

---
+++ Overtail/Assets/Scripts/NPC/INPC.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface INPC
+{
+    void movement();
+
+    public Sprite Sprite { get; set; }
+
+}
--- Overtail/Assets/Scripts/NPC/NPC.cs
+++ Overtail/Assets/Scripts/NPC/NPC.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class NPC : IInteractable
+public abstract class NPC : MonoBehaviour, IInteractable, INPC
 {
-    [SerializeField] string name;
+    [SerializeField] string NPCName;
     [SerializeField] Sprite portrait;
     [SerializeField] Sprite Sprite;
 
-    public void movementPattern()
+    Sprite INPC.Sprite { get; set; }
+
+    private Rigidbody2D rb;
+    private float moveSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void movement()
     {
 
     }
