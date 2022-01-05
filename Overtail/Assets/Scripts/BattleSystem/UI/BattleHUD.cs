7ca19e7e02f16c7f48901a67c348602a06628565
kd049 <kd049@hdm-stuttgart.de>
2022-01-05 15:30:03 +0100

    Smooth HUD btw

--- Overtail/Assets/Scripts/BattleSystem/UI/BattleHUD.cs
+++ Overtail/Assets/Scripts/BattleSystem/UI/BattleHUD.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
@@ -16,39 +17,52 @@ namespace Overtail.Battle
         public Slider hpSlider;
         public Text hpText;
 
+        private string _name;
+        private int _level = 0;
+        private float _hp = 0;
+        private float _maxHp = 0;
+
         public float HpSliderSmoothTime = 1.5f;
 
-        public void SetHUD(BattleUnit unit)
+        public void UpdateHUD(BattleUnit unit)
         {
-            nameText.text = unit.Name;
-            levelText.text = "Lvl " + unit.Level;
-            hpSlider.maxValue = unit.MaxHP;
-            hpSlider.value = unit.HP;
-            hpText.text = unit.HP + "/" + unit.MaxHP;
+            StartCoroutine(SmoothValues(unit, 2));
         }
 
-        public IEnumerator SmoothSliderUpdate(BattleUnit unit)
+        private IEnumerator SmoothValues(BattleUnit unit, float time)
         {
-            return SmoothSliderUpdate(unit, HpSliderSmoothTime);
-        }
-
-        public IEnumerator SmoothSliderUpdate(BattleUnit unit, float smoothTime)
-        {
-            var start = hpSlider.value;
-
             float timeElapsed = 0f;
 
-            Debug.Log($"Smooth HP Slider [{start} -> {unit.HP}]");
+            var prevName = _name;
+            var prevLevel = _level;
+            var prevMaxHp = _maxHp == 0 ? unit.MaxHP : _maxHp;
+            var prevHp = _hp;
 
-            while (hpSlider.value != unit.HP)
+            while (true)
             {
                 timeElapsed += Time.deltaTime;
+                yield return null;
 
-                hpSlider.value = Mathf.SmoothStep(start, unit.HP, timeElapsed/smoothTime);
-                hpText.text = (int)hpSlider.value + "/" + unit.MaxHP;
+                _name = unit.Name;
+                _level = (int)Mathf.SmoothStep(prevLevel, unit.Level, timeElapsed / time);
+                _maxHp = Mathf.SmoothStep(prevMaxHp, unit.MaxHP, timeElapsed / time);
+                _hp = Mathf.SmoothStep(prevHp, unit.HP, timeElapsed / time);
 
-                yield return new WaitForEndOfFrame();
+                ApplyGui();
+
+                if (timeElapsed > time) break;
             }
+
+            yield break;
+        }
+
+        private void ApplyGui()
+        {
+            nameText.text = _name;
+            levelText.text = "Lvl " + _level;
+            hpSlider.maxValue = _maxHp;
+            hpSlider.value = _hp;
+            hpText.text = (int)_hp + "/" + (int)_maxHp;
         }
     }
 }
