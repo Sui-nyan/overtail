6628754efaa390394bf1433939a7288f8ba6a0c2
kd049 <kd049@hdm-stuttgart.de>
2022-01-18 21:16:22 +0100

    Some ideas for chest

--- Overtail/Assets/Scripts/InteractionSystem/Chest.cs
+++ Overtail/Assets/Scripts/InteractionSystem/Chest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Overtail.Items;
 using UnityEngine;
 
 public class Chest : MonoBehaviour
@@ -14,12 +15,34 @@ public class Chest : MonoBehaviour
     void Update()
     {
         
+        if(Input.GetKeyDown(KeyCode.Space)) Magic();
     }
 
     public void OpenChest()
     {
         //spawnItem();
         //setActive = false chest object despawns
+    }
+    public void Magic()
+    {
+        // stupid stuff, but ... maybe some things are  useful, sry
+
+        Item myItem = ItemDatabase.GetFromId("overtail:potion");
+
+        ItemStack myStack = new ItemStack(myItem, 99);
+
+        bool successful = InventoryManager.Instance.PickUp(myStack);
+        if (!successful)
+        {
+            var l = Lootable.Instantiate(myItem, 123, transform.position + new Vector3(Random.value * 5 - 5, Random.value * 5 - 5, 0));
+            var rb = l.gameObject.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            Debug.LogWarning("Dropped items");
+        }
+        else
+        {
+            Debug.LogWarning("Sent items to inventory");
+        }
     }
 
 }
