f62f2e4bf8b3405ce596114a2e3550a5ac079575
Felix <fs146@hdm-stuttgart.de>
2022-02-13 19:21:16 +0100

    Add scene to pos at login

--- API/Backend/Controllers/Player/PlayerDataController.php
+++ API/Backend/Controllers/Player/PlayerDataController.php
@@ -8,7 +8,7 @@ class PlayerDataController extends Controller
 	protected function execute(): void {
 		$uuid = Auth::tokenUuid();
 		$inv = (new Query('SELECT `item` id, `amount` FROM `Inventory` WHERE `uuid`=:uuid;', [':uuid' => $uuid]))->fetchAll();
-		$pos = (new Query('SELECT `x`, `y` FROM `PlayerData` WHERE `uuid`=:uuid;', [':uuid' => $uuid]))->fetch();
+		$pos = (new Query('SELECT `x`, `y`, `scene` FROM `PlayerData` WHERE `uuid`=:uuid;', [':uuid' => $uuid]))->fetch();
 
         (new APIView([
 			'pos' => $pos,		// Player position [x, y]
